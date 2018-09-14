8467ab18a7b1330412f55dbde8a7dcea7d888775
Ricki Moore <mavrik.development@gmail.com>
2018-09-14 23:51:25 +0200

    Connect navbar to splash menu

--- resources/assets/js/components/navbar.vue
+++ resources/assets/js/components/navbar.vue
@@ -8,10 +8,17 @@
             <li>About</li>
             <li>Contact</li>
         </ul>
+        <div class="nav--mobile-btn">
+            <img src="/image/icons/menu.svg" alt="menu" @click="toggleMenu">
+        </div>
     </nav>
 </template>
 <script>
     export default {
-
+      methods: {
+        toggleMenu: function () {
+          this.$store.commit('togglePanel', !this.$store.state.isSidePanelActive);
+        }
+      }
     }
 </script>
