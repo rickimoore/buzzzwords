5794510ff10a4876cb20a1be472c1b06aab0e224
Ricki Moore <mavrik.development@gmail.com>
2018-09-19 12:29:34 +0200

    Added favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="/image/logo/favicon-buzz.ico">
         <title>Buzzword</title>
 
         <!-- Fonts -->
-        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ secure_asset('css/vue-material.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/vue-material.min.css') }}" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <script>
           window.data = <?php echo json_encode([
@@ -22,11 +23,11 @@
             <crawler-nav></crawler-nav>
             <crawler-sidepanel :state="$store.state.isSidePanelActive" :history="$store.state.history"></crawler-sidepanel>
             <crawler-input></crawler-input>
-            <div v-if="$store.state.classifications.length > 0 || $store.state.offers && $store.state.offers.length > 0" class="analytics--container">
+            <div v-if="$store.state.classifications.length > 0 || $store.state.offers && $store.state.offers.length > 0" class="buzz--container">
                 <crawler-analytics v-if="$store.state.classifications.length > 0"  :data="$store.state.classifications"></crawler-analytics>
                 <job-offer-preview v-if="$store.state.offers.length > 0" :offers="$store.state.offers"></job-offer-preview>
             </div>
         </div>
     </body>
-    <script src="{{ secure_asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </html>
