12c73c2aa2038c4c9a1fb478509eab6d8aac27ae
Ricki Moore <mavrik.development@gmail.com>
2018-09-27 14:40:59 +0200

    Added tutorial for crawler input

--- resources/assets/js/components/crawler-input.vue
+++ resources/assets/js/components/crawler-input.vue
@@ -6,7 +6,17 @@
         </div>
         <div class="crawler--input-group">
             <input type="text" placeholder="Paste your job offer here..." @paste="addToBucket" v-model="form.link">
-            <button :class="{'active--button': bucket.length > 0 || form.link}" @click="submitBucket">submit</button>
+            <button class="submit--btn" :class="{'active--button': bucket.length > 0 || form.link}" @click="submitBucket">submit</button>
+            <transition name="shortSlideUp">
+                <div v-if="isTut" class="crawler--tut">
+                    <img src="/image/tutorials/giphy--buzz.gif" alt="tut">
+                    <div class="tut--info">
+                        <p>Get started by pasting your offer below...</p>
+                        <button @click="closeTut" class="bzz--btn">Understood</button>
+                        <div class="caret"></div>
+                    </div>
+                </div>
+            </transition>
         </div>
         <div class="crawler--input-list">
             <div class="input--item" v-for="link in bucket">
@@ -21,16 +31,37 @@
 
 <script>
     export default {
+        props: ['tutPriority'],
         data: function () {
           return {
             form: {
               link: ''
             },
+            isTut: false,
             bucket: [],
             isError: false
           }
         },
+      watch: {
+        'tutPriority': function (nV) {
+          this.checkAndRelease(nV);
+        },
+        'form.link': function (nV) {
+          if(nV && this.isTut){
+            this.closeTut();
+          }
+        }
+      },
+      mounted() {
+        setTimeout(() => this.checkAndRelease(this.tutPriority), 1000);
+      },
       methods: {
+        checkAndRelease: function ($modal) {
+          var $tutCookie = this.$cookie.get('seen-tut');
+          if(!$modal && !$tutCookie){
+            setTimeout(() => this.isTut = true, 500);
+          }
+        },
         submitBucket: function () {
           this.bucket.forEach(($item) => {
             this.submitForm($item);
@@ -107,6 +138,10 @@
             '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
             '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
           return pattern.test(str);
+        },
+        closeTut: function () {
+          this.isTut = false;
+          this.$cookie.set('seen-tut', true, 365);
         }
       }
     }
