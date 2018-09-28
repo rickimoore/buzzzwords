73fe9cb416708c3b8fb2139fcd165145a486c9ce
Ricki Moore <mavrik.development@gmail.com>
2018-09-28 17:34:23 +0200

    Added new modals and toggles to navbar

--- resources/assets/js/components/navbar.vue
+++ resources/assets/js/components/navbar.vue
@@ -4,9 +4,9 @@
             <img src="/image/logo/buzzword.png" alt="buzzword.png">
         </div>
         <ul class="nav">
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about" @click.prevent="toggleModal('/about')">About</a></li>
+            <li><a href="/contact" @click.prevent="toggleModal('/contact')">Contact</a></li>
         </ul>
         <div class="nav--mobile-btn">
             <img src="/image/icons/menu.svg" alt="menu" @click="toggleMenu">
@@ -15,9 +15,22 @@
 </template>
 <script>
     export default {
+      data: function () {
+        return {
+          popUps: ['/about', '/contact']
+        }
+      },
+      mounted () {
+        if(this.popUps.indexOf(window.location.pathname) > -1) {
+          this.$store.commit('revealModal', {modal: window.location.pathname, type: 'info'});
+        }
+      },
       methods: {
         toggleMenu: function () {
           this.$store.commit('togglePanel', !this.$store.state.isSidePanelActive);
+        },
+        toggleModal: function ($location) {
+          this.$store.commit('revealModal', {modal: $location, type: 'info'});
         }
       }
     }
